9a7bd9c27f8bc85954e08412f8821ccf0d6b5af7
Rohit Malakar <rohit.malakar@subex.com>
2019-01-27 12:24:50 +0530

    Dpenedencies Deleted

--- bin/ng-rep.js
+++
@@ -1,9 +0,0 @@
-(function(app, undefined){
-	console.log('This is appjs file');
-}
-(window.app==window.app||{}));
-
-(function (custom, undefined){
-	console.log('this is customjs');
-	var a="ControllerJS";
-}(app.custom == app.custom||{}))
